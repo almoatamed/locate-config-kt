62853738f7796c2a8c0764aff8737d7fbf1d6f4a
Salem Elmotamed <s.elmotamed96@gmail.com>
2025-11-04 05:20:10 +0200

    updates

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,13 @@ let cacheMap: {
     [key: string]: ReadValueType;
 } = {};
 
+let pathsCacheMap: {
+    [key: string]: string;
+} = {};
+
 export const clearConfigCache = () => {
     cacheMap = {};
+    pathsCacheMap = {};
 };
 
 export const getConfig = async ({
@@ -62,6 +67,54 @@ export const getConfig = async ({
         const value = await readFile(p, encoding);
         cacheValue(value);
         return value;
+    }
+
+    return null;
+};
+
+export const getConfigPath = async ({
+    configFileNameWithExtension,
+    cache = true,
+    srcDirName = "src",
+}: {
+    configFileNameWithExtension: string;
+    cache?: boolean;
+    srcDirName?: string;
+    encoding?: "ascii" | "utf8" | "utf-8" | "utf16le" | "utf-16le" | "ucs2" | "ucs-2" | "base64";
+}): Promise<null | ReadValueType> => {
+    if (cache && pathsCacheMap[configFileNameWithExtension]) {
+        return pathsCacheMap[configFileNameWithExtension];
+    }
+
+    const cacheValue = (path: string) => {
+        if (cache) {
+            pathsCacheMap[configFileNameWithExtension] = path;
+        }
+    };
+
+    const projectRoot = await findRoot();
+    let p = path.join(projectRoot, configFileNameWithExtension);
+    if (existsSync(p) && (await stat(p)).isFile()) {
+        cacheValue(p);
+        return p;
+    }
+
+    p = path.join(projectRoot, srcDirName, configFileNameWithExtension);
+    if (existsSync(p) && (await stat(p)).isFile()) {
+        cacheValue(p);
+        return p;
+    }
+
+    p = path.join(projectRoot, "config", configFileNameWithExtension);
+    if (existsSync(p) && (await stat(p)).isFile()) {
+        cacheValue(p);
+        return p;
+    }
+
+    p = path.join(projectRoot, srcDirName, "config", configFileNameWithExtension);
+    if (existsSync(p) && (await stat(p)).isFile()) {
+        cacheValue(p);
+        return p;
     }
 
     return null;
