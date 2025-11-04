d6aca2bdf197259c6ab6306f7f8830e8dad9c122
Salem Elmotamed <s.elmotamed96@gmail.com>
2025-11-04 03:48:28 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,68 @@
+import findRoot from "find-root-kt";
+import { existsSync } from "fs";
+import { readFile, stat } from "fs/promises";
+import path from "path";
+type UnwrapPromise<P> = P extends Promise<infer R> ? R : P;
+type ReadValueType = UnwrapPromise<ReturnType<typeof readFile>>;
+export type ConfigFileReadType = ReadValueType;
+
+let cacheMap: {
+    [key: string]: ReadValueType;
+} = {};
+
+export const clearConfigCache = () => {
+    cacheMap = {};
+};
+
+export const getConfig = async ({
+    configFileNameWithExtension,
+    cache = true,
+    srcDirName = "src",
+    encoding = "utf-8",
+}: {
+    configFileNameWithExtension: string;
+    cache?: boolean;
+    srcDirName?: string;
+    encoding?: "ascii" | "utf8" | "utf-8" | "utf16le" | "utf-16le" | "ucs2" | "ucs-2" | "base64";
+}): Promise<null | ReadValueType> => {
+    if (cache && cacheMap[configFileNameWithExtension]) {
+        return cacheMap[configFileNameWithExtension];
+    }
+
+    const cacheValue = (value: ReadValueType) => {
+        if (cache) {
+            cacheMap[configFileNameWithExtension] = value;
+        }
+    };
+
+    const projectRoot = await findRoot();
+    let p = path.join(projectRoot, configFileNameWithExtension);
+    if (existsSync(p) && (await stat(p)).isFile()) {
+        const value = await readFile(p, encoding);
+        cacheValue(value);
+        return value;
+    }
+
+    p = path.join(projectRoot, srcDirName, configFileNameWithExtension);
+    if (existsSync(p) && (await stat(p)).isFile()) {
+        const value = await readFile(p, encoding);
+        cacheValue(value);
+        return value;
+    }
+
+    p = path.join(projectRoot, "config", configFileNameWithExtension);
+    if (existsSync(p) && (await stat(p)).isFile()) {
+        const value = await readFile(p, encoding);
+        cacheValue(value);
+        return value;
+    }
+
+    p = path.join(projectRoot, srcDirName, "config", configFileNameWithExtension);
+    if (existsSync(p) && (await stat(p)).isFile()) {
+        const value = await readFile(p, encoding);
+        cacheValue(value);
+        return value;
+    }
+
+    return null;
+};
